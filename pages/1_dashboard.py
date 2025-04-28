cf1df134d70998905aee991319dd8beed2ca71d7
Ahmed Abdalla <ahmedabdallla08@gmail.com>
2025-04-28 23:03:45 +0300

    Update 1_dashboard.py

--- pages/1_dashboard.py
+++ pages/1_dashboard.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Cars Dashboard 🚗", layout="wide")
 
 @st.cache_data
 def read():
-    return pd.read_csv(r"D:\machine_learinig\car_price\datasets\EDa.csv")
+    return pd.read_csv(r"https://raw.githubusercontent.com/ahmedabdalla00/cars-project/refs/heads/master/datasets/EDa.csv")
 
 # 🧠 New: Cache the scatter figure creation
 @st.cache_data
